445014d13b7b79567ec98a1f381a26a1fabffdcf
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-04 15:52:27 -0300

    Primeiro programa em C#

---
+++ Primeiro/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             char letra = '\u0041'; //letra recebe um valor --> Caracter unicode da letra 
+             bool completo = false;
+             char genero = 'F';
+             byte n1 = 126;
+             int n2 = 1000;
+             int n3 = 2147483647;
+             long n4 = 2147483648L;
+             float n5 = 4.5f;
+             double n6 = 4.5;
+             string nome = "Maria Green";
+             object obj1 = "Alex brown"; // E do tipo generico, pode ser usado no lugar "string", pq aceita qualquer valor. String não aceita..
+             object obj2 = 4.5f;
+             */
+
+            /*
+            // Como descobrir o valor do tipo de cada variavel:
+
+             int n7 = int.MinValue;
+             int n8 = int.MaxValue;
+             sbyte n9 = sbyte.MinValue;
+             sbyte n10 = sbyte.MaxValue;
+             decimal n11 = decimal.MaxValue;
+             */
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Efetuando um teste no console:");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+
+            /*
+            Console.WriteLine(letra);
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+
+            Console.WriteLine(String.Empty);
+            Console.WriteLine(n7);
+            Console.WriteLine(n8);
+            Console.WriteLine(n9);
+            Console.WriteLine(n10);
+            Console.WriteLine(n11);
+            */
+
+
+        }
+    }
+}
+
+
+
+
